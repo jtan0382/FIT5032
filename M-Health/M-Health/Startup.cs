ad18d5348fa936cd73a9bdbda992be09303af62a
jehezkiel1699 <jehezkiel1699@gmail.com>
2023-08-29 03:06:15 +1000

    Layout, Home UI
    Making user interfaces for layout and home

---
+++ M-Health/M-Health/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M_Health.Startup))]
+namespace M_Health
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- MHealth/MHealth/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MHealth.MvcApplication" Language="C#" %>
