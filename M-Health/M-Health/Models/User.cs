3bfc14b6936629c57f2664c52abd9d24b1e3171e
jehezkiel1699 <jehezkiel1699@gmail.com>
2023-08-29 20:53:19 +1000

    User Login
    login

---
+++ M-Health/M-Health/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M_Health.Entities
+{
+    public class User
+    {
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+        public int UserRole { get; set; }
+
+    }
+}
