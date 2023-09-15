5ac11b9e3d94e37c8577047ebd02c404b1f441e9
jehezkiel1699 <jehezkiel1699@gmail.com>
2023-09-15 19:01:00 +1000

    Week 7 Login and Send Mail
    Login by google and send mail by using google services

---
+++ Week7_Google/Week7_Google/Controllers/EmailController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+public class EmailController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult SendEmail(string recipient, string subject, string body)
+    {
+        try
+        {
+            // Gmail SMTP settings
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("jehezkiel1699@gmail.com", "urzxhdcaifigtoly"),
+                EnableSsl = true,
+            };
+
+            // Create a new email message
+            var message = new MailMessage("jehezkiel1699@gmail.com", recipient)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            // Send the email
+            smtpClient.Send(message);
+
+            ViewBag.Message = "Email sent successfully!";
+        }
+        catch (Exception ex)
+        {
+            ViewBag.Error = "An error occurred: " + ex.Message;
+        }
+
+        return View("Index");
+    }
+}
---
+++ Week7_Google/Week7_Google/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week7_Google.Startup))]
+namespace Week7_Google
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
